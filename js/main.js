7bb50a24c7e3aa390729bb7e7b53db9124c9ad72
A Zhila <aleksej.zhila@gmail.com>
2021-07-07 17:02:45 +0300

    добавляет вывод карточек, фильтрацию по категориям и замену заголовка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // выбор города
 
 const headerCityButton = document.querySelector('.header__city-button');
+// let hash = location.hash.substring(1);
 
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
@@ -62,15 +63,92 @@ const getData = async () => {
   }
 };
 
-const getGoods = (callback) => {
+const getGoods = (callback, value) => {
   getData()
-    .then(data => callback(data))
+    .then((data) => {
+      if (value) {
+        callback(data.filter(item => item.category === value));
+      } else {
+        callback(data);
+      }
+    })
     .catch(err => console.error(err));
 };
 
-getGoods((data) => {
-  console.warn(data);
-});
+// вывод товаров
+
+try {
+  const goodsList = document.querySelector('.goods__list');
+
+  if (!goodsList) {
+    throw 'Это не страница с товарами';
+  }
+
+  const createCard = (data) => {
+    const {
+      id, 
+      preview, 
+      cost, 
+      brand, 
+      name, 
+      sizes
+    } = data;
+    
+    const listItem = document.createElement('li');
+
+    listItem.classList.add('goods__item');
+
+    listItem.innerHTML = `
+      <article class="good">
+        <a class="good__link-img" href="card-good.html#${id}">
+          <img class="good__img" src="goods-image/${preview}" alt="${name}">
+        </a>
+        <div class="good__description">
+          <p class="good__price">${cost} &#8381;</p>
+          <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+          ${sizes ? 
+            `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>`
+             : 
+             ``}
+          <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+        </div>
+      </article>
+    `;
+
+    return listItem;
+  };
+
+  const renderGoodsList = (data) => {
+    goodsList.textContent = '';
+
+    for (const item of data) {
+      const card = createCard(item);
+      goodsList.append(card);
+    }
+  };
+
+  window.addEventListener('hashchange', () => {
+    let hash = location.hash.substring(1);
+    const title = document.querySelector('.goods__title');
+
+    switch (hash) {
+      case 'men':
+        title.textContent = 'Мужчинам';
+        break;
+      case 'women':
+        title.textContent = 'Женщинам';
+        break;
+      case 'kids':
+        title.textContent = 'Детям';
+        break;
+    }
+    
+    getGoods(renderGoodsList, hash);
+  });
+
+} catch (err) {
+  console.warn(err);
+}
 
 // слушатели
 
