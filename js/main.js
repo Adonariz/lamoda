32b801fad9f4921e80725121a3acdd9b928b2a38
A Zhila <aleksej.zhila@gmail.com>
2021-07-07 14:15:33 +0300

    добавляет получение данных с сервера

--- js/main.js
+++ js/main.js
@@ -48,6 +48,32 @@ const enableScroll = () => {
   });
 };
 
+// запрос из базы данных
+
+const database = 'db.json';
+
+const getData = async () => {
+  const data = await fetch(database);
+  
+  if(data.ok) {
+    return data.json();
+  } else {
+    throw new Error(`Данные не были получены, ошибка ${data.status} ${data.statusText}`);
+  }
+};
+
+const getGoods = (callback) => {
+  getData()
+    .then(data => callback(data))
+    .catch(err => console.error(err));
+};
+
+getGoods((data) => {
+  console.warn(data);
+});
+
+// слушатели
+
 subheaderCart.addEventListener('click', openCartModal);
 
 cartOverlay.addEventListener('click', (evt) => {
